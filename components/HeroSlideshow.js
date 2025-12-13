7f8b6089711bca8c02e5fa9a4d2b527691c3b045
bypassjack-rgb <bypassjack@gmail.com>
2025-12-14 02:42:14 +0700

    Update HeroSlideshow.js

--- components/HeroSlideshow.js
+++ components/HeroSlideshow.js
@@ -2,6 +2,7 @@
 
 import { useState, useEffect } from 'react';
 import Image from 'next/image';
+import { FaLine, FaPhoneAlt } from 'react-icons/fa';
 import styles from '../app/home.module.css';
 
 // รายการรูปภาพสำหรับ Slideshow
